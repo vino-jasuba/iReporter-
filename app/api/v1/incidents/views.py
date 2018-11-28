bf5f8f7aae8e9e0e1582823649d6d98862138eb1
vino <ovincent7949@gmail.com>
2018-11-28 12:11:29 +0300

    [Starts #162261963] create endpoints for fetching incident reports

--- app/api/v1/incidents/views.py
+++ app/api/v1/incidents/views.py
@@ -4,12 +4,33 @@ from flask_restful import Api, Resource
 from app.api.v1.common.api_response import ApiResponse
 from .models import IncidentModel
 
-incident_list = []
+incident_list = [] 
+
+class Incident(Resource, ApiResponse):
+    
+    def __init__(self):
+        self.db = IncidentModel(incident_list)
+
+    
+    def get(self, incident_id):
+        incident = self.db.find(incident_id)
+
+        if not incident:
+            return self.respondNotFound()
+        
+        return incident, 200
+        
 
 class IncidentList(Resource):
     
     def __init__(self):
         self.db = IncidentModel(incident_list)
+    
+    def get(self):
+        return {
+            'status': 200,
+            'data': self.db.all()
+            }, 200
 
     def post(self):
 
@@ -31,3 +52,13 @@ class IncidentList(Resource):
         return {'message': 'Successfully created incident report'}, 201
 
 
+
+class IncidenceQuery(Incident):
+
+    def get(self, incident_type):
+        red_flags = self.db.where('type', incident_type).get()
+
+        return {
+            'data': red_flags,
+            'status': 200
+        }
