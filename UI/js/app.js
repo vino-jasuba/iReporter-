5d0da8539d2c8df6463a3ae516898d7d5e628ee6
vino <ovincent7949@gmail.com>
2019-01-14 15:42:44 +0300

    [Finishes #163189787] clean up UI elements

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,58 +1,55 @@
-locationObject = {}
+let locationObject = {}
 
-baseUrl = 'http:127.0.0.1:5000/api/v2/'
+let baseUrl = 'http:127.0.0.1:5000/api/v2/'
 
-function locationSearch() {
-    input = document.getElementsByName('location')[0]
-    options = {
-        componentRestrictions: {country: 'ke'}
-    }
+function locationSearch () {
+  let input = document.getElementsByName('location')[0]
+  let options = {
+    componentRestrictions: { country: 'ke' }
+  }
 
-    autocomplete = new google.maps.places.Autocomplete(input, options);
-    
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
-    google.maps.event.addListener(autocomplete, 'place_changed', function(){
-        place = autocomplete.getPlace();
-        locationObject.lat = place.geometry.location.lat()
-        locationObject.lng = place.geometry.location.lng()
-        locationObject.city = place.formatted_address
+  let autocomplete = new google.maps.places.Autocomplete(input, options)
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+    let place = autocomplete.getPlace()
+    locationObject.lat = place.geometry.location.lat()
+    locationObject.lng = place.geometry.location.lng()
+    locationObject.city = place.formatted_address
 
-        console.log(locationObject)
-    })
+    console.log(locationObject)
+  })
 }
 
 /**
- * 
- * @param {*} table 
- * @param {*} columns 
- * @param {*} collection 
- * 
+ *
+ * @param {*} table
+ * @param {*} columns
+ * @param {*} collection
+ *
  */
-function renderTableContent(table, columns, collection) {
-    table = document.getElementById(table)
-    counter = 1
-    for(item of collection) {
-        
-        row = table.tBodies[0].insertRow(-1)
-        row.classList.add("font-monseratt", "fs-sm", "grey-light")
+function renderTableContent (table, columns, collection) {
+  table = document.getElementById(table)
+  let counter = 1
+  for (item of collection) {
+    let row = table.tBodies[0].insertRow(-1)
+    row.classList.add('font-monseratt', 'fs-sm', 'grey-light')
 
-        columns.map(i => {
-            tCell = row.insertCell(-1)
+    columns.map(i => {
+      let tCell = row.insertCell(-1)
 
-            tCell.innerHTML = item[i]
+      tCell.innerHTML = item[i]
 
-            if (i == 'id') {
-                tCell.innerHTML = counter
-                counter++
-            }
+      if (i === 'id') {
+        tCell.innerHTML = counter
+        counter++
+      }
 
-            if (i == 'location') {
-                tCell.innerHTML = item[i].city
-            }
-        })
+      if (i === 'location') {
+        tCell.innerHTML = item[i].city
+      }
+    })
 
-        tControls = row.insertCell(-1)
-        tControls.innerHTML = `<div class="flex">
+    let tControls = row.insertCell(-1)
+    tControls.innerHTML = `<div class="flex">
             <span class="px-sm">
                 <a class="pointer" onclick="viewRecord(${item.id})"><i class="fas fa-eye"></i></a>
             </span>
@@ -62,119 +59,160 @@ function renderTableContent(table, columns, collection) {
             <span class="px-sm">
                 <a class="pointer" onclick="deleteRecord(${item.id})"><i class="fas fa-trash"></i></a>
             </span>
-            </div>` 
-    }
-} 
-
-function authUser() {
-    return JSON.parse(localStorage.getItem('auth_user'))
+            </div>`
+  }
 }
 
-function userProfile() {
-    get(baseUrl + 'auth/user/profile')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        localStorage.setItem('auth_user', JSON.stringify(data))
-        document.getElementById('name').innerHTML = data.firstname + ' ' + data.lastname
-        document.getElementById('email').innerHTML = data.email
-        document.getElementById('user_role').innerHTML = data.isAdmin? 'Admin' : 'User'
-    })
-    .catch(errors => {
-        console.log(errors)
-    })
+function checkAuth () {
+  if (authUser()) return
+
+  window.location.href = 'login.html'
 }
 
-function viewRecord(id) {
-    console.log("view record", id)
+function authUser () {
+  return JSON.parse(window.localStorage.getItem('auth_user'))
+}
 
-    get(baseUrl + `incidents/${id}`)
+function logout () {
+  window.localStorage.removeItem('auth_user');
+  window.location = 'index.html'
+}
+
+function loadUserProfile () {
+  return get(baseUrl + 'auth/user/profile')
     .then(response => response.json())
     .then(data => {
-        console.log(data.data)
-        localStorage.setItem('current_incident', JSON.stringify(data.data))
-        window.location = 'view_record.html'
+      window.localStorage.setItem('auth_user', JSON.stringify(data))
+      return data
     })
 }
 
-function editRecord(id) {
-    console.log("edit record", id)
+function viewRecord (id) {
+  console.log('view record', id)
 
-    get(baseUrl + `incidents/${id}`)
+  get(baseUrl + `incidents/${id}`)
     .then(response => response.json())
     .then(data => {
-        localStorage.setItem('current_incident', JSON.stringify(data.data))
-        window.location = 'edit_record.html'
+      console.log(data.data)
+      window.localStorage.setItem('current_incident', JSON.stringify(data.data))
+      window.location = 'view_record.html'
     })
 }
 
-function deleteRecord(id) {
-    console.log("delete record", id)
+function editRecord (id) {
+  console.log('edit record', id)
 
-    deleteData(baseUrl + `incidents/${id}`)
+  get(baseUrl + `incidents/${id}`)
     .then(response => response.json())
     .then(data => {
-        window.location = 'dashboard.html'
+      window.localStorage.setItem('current_incident', JSON.stringify(data.data))
+      window.location = 'edit_record.html'
+    })
+}
+
+function deleteRecord (id) {
+  console.log('delete record', id)
+
+  deleteData(baseUrl + `incidents/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      window.location = 'dashboard.html'
     }).catch(error => {
-        console.log(error)
+      console.log(error)
     })
 }
 
-function getHeaders() {
-    return {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + localStorage.getItem('auth_token')
-    }
+function getHeaders () {
+  return {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + window.localStorage.getItem('auth_token')
+  }
 }
 
-async function get(url) {
-    const response = await fetch(url, {
-        method: 'get',
-        headers:getHeaders()
-    });
+async function get (url) {
+  const response = await fetch(url, {
+    method: 'get',
+    headers: getHeaders()
+  })
 
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response
 }
 
+async function deleteData (url) {
+  const response = await fetch(url, {
+    method: 'delete',
+    headers: getHeaders()
+  })
 
-async function deleteData(url) {
-    const response = await fetch(url, {
-        method: 'delete',
-        headers:getHeaders()
-    });
-
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response
 }
 
-async function postData(url, data) {
-    const response = await fetch(url, {
-        method: 'post',
-        body: JSON.stringify(data),
-        headers: getHeaders()
-    });
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+async function postData (url, data) {
+  const response = await fetch(url, {
+    method: 'post',
+    body: JSON.stringify(data),
+    headers: getHeaders()
+  })
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response
 }
 
-
-async function patch(url, data) {
-    const response = await fetch(url, {
-        method: 'patch',
-        body: JSON.stringify(data),
-        headers: getHeaders()
-    });
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+async function patch (url, data) {
+  const response = await fetch(url, {
+    method: 'patch',
+    body: JSON.stringify(data),
+    headers: getHeaders()
+  })
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response
 }
 
+function signup () {
+  let signupUrl = baseUrl + 'auth/signup'
 
+  let username = document.getElementsByName('username')[0].value
+  let password = document.getElementsByName('password')[0].value
+  let email = document.getElementsByName('email')[0].value
+  let firstname = document.getElementsByName('firstname')[0].value
+  let lastname = document.getElementsByName('lastname')[0].value
+
+  let formData = {
+    username,
+    password,
+    email,
+    firstname,
+    lastname
+  }
+
+  let request = new Request(signupUrl, {
+    method: 'post',
+    body: JSON.stringify(formData),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  fetch(request)
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.statusText)
+      }
+
+      return response
+    }).then(response => response.json())
+    .then(data => {
+      console.log(data)
+      window.localStorage.setItem('user', data.data)
+      window.location = 'login.html'
+    }).catch(error => {
+      console.log(error)
+    })
+}
